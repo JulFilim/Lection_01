f18d6940b4dc35af5da9806a9c052429f190e8c1
JulFilim <julfilim@mail.ru>
2023-04-21 12:14:42 +0300

    If_else

---
+++ ex005_if_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("input name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
